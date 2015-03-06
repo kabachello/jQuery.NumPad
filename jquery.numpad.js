5b3afc5520e17871d9e8aa55c95630d37987c1da
Andrej Kabachnik <andrej_ka@gmx.de>
2015-03-06 09:17:48 +0100

    Working with multiple elements now

--- jquery.numpad.js
+++ jquery.numpad.js
@@ -1,39 +1,20 @@
 (function($){
     $.fn.numpad=function(options){
-
-		var defaults = {
-            target: false,
-			openOnEvent: 'click',
-			backgroundTpl: '<div></div>',
-			displayTpl: '<input type="number" />',
-			displayCellTpl: '<td colspan="4"></td>',
-			rowTpl: '<tr></tr>',
-			cellTpl: '<td></td>',
-			buttonNumberTpl: '<button></button>',
-			buttonFunctionTpl: '<button></button>',
-			gridTableClass: '',
-			textDone: 'Done',
-			textDelete: 'Del',
-			textClear: 'Clear',
-			textCancel: 'Cancel',
-			appendNumPadTo: $(document.body)
-        };
 		
-		options = $.extend({}, defaults, options);
+		options = $.extend({}, $.fn.numpad.defaults, options);
 		
 		return this.each(function(){
 			$(this).attr("readonly", true);
-			$(this).unbind(options.openOnEvent);
 			$(this).bind(options.openOnEvent,function(){
-				nmpd.open();
+				nmpd.open(options.target ? options.target : $(this));
 			});
 
 			if ($('#nmpd').length == 0) {
 				var nmpd = $('<div id="nmpd"></div>').css('display', 'none');
-				var table = $('<table class="nmpd-grid '+options.gridTableClass+'" id="n_keypad">');
+				var table = $(options.gridTpl).addClass('nmpd-grid');
 				var display = $(options.displayTpl).addClass('nmpd-display').attr('id', 'nmpd-display');
 				nmpd.display = display;
-				table.append($(options.rowTpl).append($(options.displayCellTpl).append(display)));			
+				table.append($(options.rowTpl).append($(options.displayCellTpl).append(display).append($('<input type="hidden" class="dirty" value="0"></input>'))));			
 				table.append(
 					$(options.rowTpl)
 						.append($(options.cellTpl).append($(options.buttonNumberTpl).html(7).addClass('numero')))
@@ -56,7 +37,7 @@
 						.append($(options.cellTpl).append($(options.buttonNumberTpl).html(2).addClass('numero')))
 						.append($(options.cellTpl).append($(options.buttonNumberTpl).html(3).addClass('numero')))
 						.append($(options.cellTpl).append($(options.buttonFunctionTpl).html(options.textCancel).addClass('cancel').click(function(){
-							nmpd.close(true);
+							nmpd.close(false);
 						})))
 					).append(
 					$(options.rowTpl)
@@ -79,46 +60,75 @@
 						})))
 						.append($(options.cellTpl).append($(options.buttonFunctionTpl).html(options.textDone).addClass('done')))
 					);
-				nmpd.append($(options.backgroundTpl).addClass('nmpd-overlay').attr('id', 'nmpd-overlay'));
+				nmpd.append($(options.backgroundTpl).addClass('nmpd-overlay').attr('id', 'nmpd-overlay').click(function(){nmpd.close(false)}));
 				nmpd.append(table);
-				options.appendNumPadTo.append(nmpd).enhanceWithin();    
-			
-				$('.numero').bind('click', function(){
-					display.val(display.val() + $(this).text());
+				if (options.onKeypadCreate){
+					nmpd.on('numpad.create', options.onKeypadCreate);
+				}
+				(options.appendKeypadTo ? options.appendKeypadTo : $(document.body)).append(nmpd);   
+				
+				$('#nmpd .numero').bind('click', function(){
+					if ($('#nmpd .dirty').val() == '0'){
+						display.val($(this).text());
+						$('#nmpd .dirty').val(1);
+					} else {
+						display.val(display.val() + $(this).text());
+					}
 				});
+				
+				nmpd.trigger('numpad.create');
 			} else {
 				nmpd = $('#nmpd');
 				nmpd.display = $('#nmpd input.nmpd-display');
 			}
-			
-			nmpd.target = options.target ? options.target : $(this);
-			$('#nmpd .done').click(function(){ nmpd.close(false); });
 
-			nmpd.close = function(discardInput){
-				if (discardInput !== true){
-					if (nmpd.target.prop("tagName") == 'INPUT'){
-						nmpd.target.val(nmpd.display.val());
+			nmpd.close = function(target){
+				if (target){
+					if (target.prop("tagName") == 'INPUT'){
+						target.val(nmpd.display.val());
 					} else {
-						nmpd.target.html(nmpd.display.val());
+						target.html(nmpd.display.val());
 					}
 				} 
 				nmpd.hide('fast');
 				return nmpd;
 			}
 			
-			nmpd.open = function(initialValue){
+			nmpd.open = function(target, initialValue){
 				if (initialValue){
 					nmpd.display.val(initialValue);
 				} else {
-					if (nmpd.target.prop("tagName") == 'INPUT'){
-						nmpd.display.val(nmpd.target.val());
+					if (target.prop("tagName") == 'INPUT'){
+						nmpd.display.val(target.val());
 					} else {
-						nmpd.display.val(parseFloat(nmpd.target.html()));
+						nmpd.display.val(parseFloat(target.text()));
 					}
 				}
+				$('#nmpd .dirty').val(0);
 				nmpd.show('fast');
+				$('#nmpd .done').one('click', function(){ nmpd.close(target); });
 				return nmpd;
 			}		  
 		});
     }
+	
+	$.fn.numpad.defaults = {
+		target: false,
+		openOnEvent: 'click',
+		backgroundTpl: '<div></div>',
+		gridTpl: '<table></table>',
+		displayTpl: '<input type="number" />',
+		displayCellTpl: '<td colspan="4"></td>',
+		rowTpl: '<tr></tr>',
+		cellTpl: '<td></td>',
+		buttonNumberTpl: '<button></button>',
+		buttonFunctionTpl: '<button></button>',
+		gridTableClass: '',
+		textDone: 'Done',
+		textDelete: 'Del',
+		textClear: 'Clear',
+		textCancel: 'Cancel',
+		appendKeypadTo: false,
+		onKeypadCreate: false
+	};
 })(jQuery);
